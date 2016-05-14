cab4ccc8624f7048f1a0e48f77d146abf4f84836
uktechreviews <spencerorgan1@me.com>
2016-05-14 17:00:09 +0100

    Update safe_steps.py
    fixed error

--- safe_steps.py
+++ safe_steps.py
@@ -6,6 +6,6 @@ while True:
 	p = mc.player.getTilePos()
 	b = mc.getBlock(p.x, p.y-1, p.z)
 	if b == block.AIR.id or b == block.WATER_FLOWING.id or b==block.WATER_STATIONARY.id:
-		mc.setBlock(pos.x, pos.y-1, pos.z, block.WOOD_PLANKS.id)
+		mc.setBlock(p.x, p.y-1, p.z, block.WOOD_PLANKS.id)
 
 
