293d6888b0a836533dadc2778d95effc503fd4ca
uktechreviews <spencerorgan1@me.com>
2016-05-14 16:59:39 +0100

    Update first_steps.py
    fixed bug with copy and paste

--- first_steps.py
+++ first_steps.py
@@ -2,8 +2,8 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 import time
 mc = minecraft.Minecraft.create()
-mc.postToChat(“Hello World, Steve here!”)
-mc.postToChat(“Is that a creeper behind you?”)
+mc.postToChat("Hello World, Steve here!")
+mc.postToChat("Is that a creeper behind you?")
 p = mc.player.getPos()
 mc.player.setPos (p.x, p.y+10, p.z)
 tile = mc.player.getTilePos()
