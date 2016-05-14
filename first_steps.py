6e1e0a83a7900115469a0a4986e07d3c6760c1a9
uktechreviews <spencerorgan1@me.com>
2016-05-14 16:48:54 +0100

    Create first_steps.py

---
+++ first_steps.py
@@ -0,0 +1,10 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+mc = minecraft.Minecraft.create()
+mc.postToChat(“Hello World, Steve here!”)
+mc.postToChat(“Is that a creeper behind you?”)
+p = mc.player.getPos()
+mc.player.setPos (p.x, p.y+10, p.z)
+tile = mc.player.getTilePos()
+mc.setBlock (tile.x, tile.y, tile.z,block.SNOW.id)
